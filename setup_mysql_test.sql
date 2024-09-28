c8547308ec3d79ad5697a56fcc5b620f8c25818e
MariamIssah <m.issah1@alustudent.com>
2024-09-28 09:05:22 +0000

    edit

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,14 @@
- test script that prepares a MySQL server for the project
+-- Create the hbnb_test_db database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user hbnb_test if not exists and set the password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
-GRANT USAGE ON `*.*` TO 'hbnb_test'@'localhost'
+
+-- Grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
