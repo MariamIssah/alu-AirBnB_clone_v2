b2f7b2da88e7a5d756921b30f5d9ecf5c49ae3a3
MariamIssah <m.issah1@alustudent.com>
2024-09-28 13:09:30 +0000

    all files committed

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
+import os
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
@@ -8,8 +8,8 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    '''amenity class'''
+    """Amenities of a place"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         place_amenities = relationship('Place', secondary="place_amenity")
