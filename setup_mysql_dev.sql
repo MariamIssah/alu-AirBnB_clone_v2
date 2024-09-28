b05de896d7cbabe51ccfd999bd14bfd3cd607ffd
MariamIssah <m.issah1@alustudent.com>
2024-09-28 08:35:06 +0000

    editted

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,14 @@
-te the data base if it doesnt exit
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create a new use in the localhost COMMENT
+-- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grantt the user hbnb_dev all privileges on the database hbnb_dev_db
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- Grant all Select Privileges on performance_schema to hbnb_dev
+-- Grant SELECT privileges on performance_schema to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Apply the changes (flush privileges)
+FLUSH PRIVILEGES;
