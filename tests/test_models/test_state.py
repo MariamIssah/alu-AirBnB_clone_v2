5c0574c5fcfc0822afb92e1831dfc9a71e11e616
MariamIssah <m.issah1@alustudent.com>
2024-09-28 19:53:40 +0000

    test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,25 +1,20 @@
-import unittest
+#!/usr/bin/python3
+""" """
+from tests.test_models.test_base_model import TestBaseModel
 from models.state import State
 
-class TestState(unittest.TestCase):
-    """ Class to test the State model """
 
-    def test_name_initialization(self):
-        """ Test name attribute initialization with default value """
-        new = State()
+class TestState(TestBaseModel):
+    """Test for state """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = "California"
+        self.value = State
+
+    def test_name3(self):
+        """ """
+        new = self.value()
+        new.name = "Arizona"
         self.assertEqual(type(new.name), str)
-        self.assertEqual(new.name, "")  # Ensure it defaults to an empty string
-
-    def test_name_assignment(self):
-        """ Test name attribute when explicitly assigned """
-        new = State(name="California")
-        self.assertEqual(type(new.name), str)
-        self.assertEqual(new.name, "California")  # Check the assigned value
-
-    def test_name_none(self):
-        """ Test name attribute when None is assigned """
-        new = State(name=None)
-        self.assertEqual(type(new.name), str)  # Should still be str
-        self.assertEqual(new.name, "")  # Assuming we want None to default to an empty string
-
-# You can add more tests for other attributes and methods
