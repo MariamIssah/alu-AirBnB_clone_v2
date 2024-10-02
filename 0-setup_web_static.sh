263ab1d2b5c88afbd1878ba0b443f5f4389b23ba
MariamIssah <m.issah1@alustudent.com>
2024-10-02 00:02:19 +0000

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,8 +19,8 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Give ownership to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx config
-sudo sed -i '/^\tserver_name _;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+# Update Nginx config with new location block for /hbnb_static
+sudo sed -i '/^\tserver_name _;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /alu-AirBnB_clone_v2/web_static/;\n\t\tindex index.html;\n\t}' /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
